e55ae0b5e82174fa82ba5039fd55307f68289ab7
mahadee hasan abir <37393027+mahade10@users.noreply.github.com>
2020-03-03 21:46:42 +0600

    complete

--- roundRobin.cpp
+++ roundRobin.cpp
@@ -1,75 +1,76 @@
-#include <iostream>
-#include <algorithm>
-#include <cstring>
+#include<bits\stdc++.h>
 using namespace std;
+void waiting(int n,int q,int p[],int b[],int wt[]){
+    int rem_bt[n],i,t;
+    for(i=0;i<n;i++){
+        rem_bt[i]=b[i];
+    }
+    t=0;
+    while(1){
+        bool done=true;
+        for(i=0;i<n;i++){
+            if(rem_bt[i]>0){
+                done=false;
+                if(rem_bt[i]>q){
+                    t=t+q;
+                    rem_bt[i]=rem_bt[i]-q;
+                }
+                else{
+                    t=t+rem_bt[i];
+                    wt[i]=t-b[i];
+                    rem_bt[i]=0;
+                }
+            }
+        }
+        if(done==true){
+            break;
+        }
+    }
 
-typedef struct proccess
-{
-	int bt,ct,wt,btt;
-	string pro_id;
-	//bt->burst time
-	//ct->compete time
-	//wt->waiting time
-	//btt->copy of bt
+}
 
-}Schedule;
+void turnaround(int n,int p[],int b[],int wt[],int tat[]){
+    for(int i=0;i<n;i++){
+        tat[i]=b[i]+wt[i];
+    }
+}
 
+void avgtime(int n,int q,int p[],int b[]){
+    int i,wt[n],tat[n],t_wt=0,t_tat=0;
+    waiting(n,q,p,b,wt);
+    turnaround(n,p,b,wt,tat);
+
+    cout<<"process   "<<"burst time  "<<"waiting time  "<<"turnaround time "<<endl;
+    for(i=0;i<n;i++){
+        t_wt=t_wt+wt[i];
+        t_tat=t_tat+tat[i];
+        cout<<"p"<<p[i]<<"\t\t"<<b[i]<<"\t\t"<<wt[i]<<"\t\t"<<tat[i]<<endl;
+    }
+
+    cout<<"avarage waitting time: "<<(float)t_wt/(float)n<<endl;
+    cout<<"avarage turnaround time: "<<(float)t_tat/(float)n<<endl;
+}
 
 int main()
 {
-	Schedule pro[10];
-	//An array of Processes
-	int n,i,j,pcom,q;
-	//n = number of processes, i= iteration variable,pcom=number of complete program,q=quantum time
+    int i,n,p[20],b[20],q;
 
-	cout<<"Enter the number of Process::";
-	cin>>n;
+    cout<<"Enter the Quantum Number: ";
+    cin>>q;
+    cout<<endl;
 
-	cout<<"Enter the value of quantum time::";
-	cin>>q;
+    cout<<"Enter the number of process: ";
+    cin>>n;
+    cout<<endl;
 
-	cout<<"Enter the Process id  burst time :::";
+    cout<<"Enter the process & burst time: ";
+    cout<<endl;
 
-	for(i=0;i<n;i++)
-	{
-		cin>>pro[i].pro_id;
-		cin>>pro[i].bt;
-		pro[i].btt=pro[i].bt;
-	}
-
-	int ctime=0;//ctime=current time
-	pcom=0;
-	while(pcom<n)
-	{
-		for(j=0;j<n;j++)
-		{
-			if(pro[j].bt>q){
-                pro[j].bt-=q;
-                ctime+=q;
-			}
-			else if(pro[j].bt>0){
-
-                ctime+=pro[j].bt;
-                pro[j].ct=ctime;
-                pcom=pcom+1;
-                pro[j].bt=0;
-			}
-		}
-	}
-     double AverageWaiting=0;
-	cout<<"ProID\tBurst time\tComplete time\tWaiting time\n";
-
-	for(i=0;i<n;i++)
-	{
-
-		pro[i].wt=pro[i].ct-pro[i].btt;
-		AverageWaiting+=pro[i].wt;
-
-		cout<<pro[i].pro_id<<"\t"<<pro[i].btt<<"\t"<<pro[i].ct<<"\t"<<pro[i].wt<<endl;
-
-	}
-	cout<<"Average waiting time :"<<AverageWaiting/n<<endl;
-
-	return 0;
+    for(i=0;i<n;i++){
+        cout<<"p";
+        cin>>p[i];
+        cin>>b[i];
+    }
+    avgtime(n,q,p,b);
+    return 0;
 }
-
